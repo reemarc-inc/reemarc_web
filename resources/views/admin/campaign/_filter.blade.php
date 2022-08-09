5ba0a98e1238f08d74046a2eeebf3eeb85f51aa2
jilee2 <jilee2@kissusa.com>
2022-08-09 17:14:41 -0400

    fix bug on search function

--- resources/views/admin/campaign/_filter.blade.php
+++ resources/views/admin/campaign/_filter.blade.php
@@ -1,7 +1,7 @@
 <form method="GET" action="{{ route('campaign.index') }}">
     <div class="form-row">
         <div class="form-group col-md-4">
-        <input type="text" name="q" class="form-control" id="q" placeholder="Search by Project Name, Created By" value="{{ !empty($filter['q']) ? $filter['q'] : '' }}">
+        <input type="text" name="q" class="form-control" id="q" placeholder="Search by Project Name" value="{{ !empty($filter['q']) ? $filter['q'] : '' }}">
             <input type="hidden" name="status" id="status" value="active">
         </div>
 {{--        <div class="form-group col-md-2">--}}
