161e45f7758ab19e16547aca5b26770be187b19b
jilee2 <jilee2@kissusa.com>
2022-10-07 12:04:01 -0400

    favicon issue

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
         <meta name="description" content="{{ config('app.name', 'Command Center') }}">
-        <title>{{ config('app.name', 'Command Center') }}</title>
+        <title>KPM - KISS Omnichannel Project Manager</title>
 
         <!-- General CSS Files -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -27,6 +27,7 @@
         <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
         <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
         <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/storage/campaigns/1/koe-favicon.png') }}">
 
         <style>
             .btn-filter {
