f23d1f85e1c7785522f2b7563b13f8a08112a73a
jilee2 <jilee2@kissusa.com>
2022-10-24 17:03:10 -0400

    update for status board (copy writer)

--- resources/views/admin/asset/jira_copywriter.blade.php
+++ resources/views/admin/asset/jira_copywriter.blade.php
@@ -31,22 +31,22 @@
                             $start_late_css = "blink-bg" ;
 
                             if($asset->asset_type == 'email_blast'){
-                                $start_date = date('m/d/Y', strtotime($asset->due . ' -14 weekday'));
+                                $start_date = date('m/d/Y', strtotime($asset->due . ' -16 weekday'));
                                 if($start_date <= date('m/d/Y') ){
                                     $start_css = $start_late_css;
                                 }
                             }else if($asset->asset_type == 'website_banners'){
-                                $start_date = date('m/d/Y', strtotime($asset->due . ' -15 weekday'));
+                                $start_date = date('m/d/Y', strtotime($asset->due . ' -17 weekday'));
                                 if($start_date <= date('m/d/Y') ){
                                     $start_css = $start_late_css;
                                 }
                             }else if($asset->asset_type == 'social_ad'){
-                                $start_date = date('m/d/Y', strtotime($asset->due . ' -14 weekday'));
+                                $start_date = date('m/d/Y', strtotime($asset->due . ' -16 weekday'));
                                 if($start_date <= date('m/d/Y') ){
                                     $start_css = $start_late_css;
                                 }
                             }else if($asset->asset_type == 'landing_page'){
-                                $start_date = date('m/d/Y', strtotime($asset->due . ' -28 weekday'));
+                                $start_date = date('m/d/Y', strtotime($asset->due . ' -30 weekday'));
                                 if($start_date <= date('m/d/Y') ){
                                     $start_css = $start_late_css;
                                 }
@@ -56,17 +56,17 @@
                                     $start_css = $start_late_css;
                                 }
                             }else if($asset->asset_type == 'a_content'){
-                                $start_date = date('m/d/Y', strtotime($asset->due . ' -25 weekday'));
+                                $start_date = date('m/d/Y', strtotime($asset->due . ' -27 weekday'));
                                 if($start_date <= date('m/d/Y') ){
                                     $start_css = $start_late_css;
                                 }
                             }else if ($asset->asset_type == 'programmatic_banners'){
-                                $start_date = date('m/d/Y', strtotime($asset->due . ' -17 weekday'));
+                                $start_date = date('m/d/Y', strtotime($asset->due . ' -19 weekday'));
                                 if($start_date <= date('m/d/Y') ){
                                     $start_css = $start_late_css;
                                 }
                             }else if($asset->asset_type == 'misc'){
-                                $start_date = date('m/d/Y', strtotime($asset->due . ' -13 weekday'));
+                                $start_date = date('m/d/Y', strtotime($asset->due . ' -15 weekday'));
                                 if($start_date <= date('m/d/Y') ){
                                     $start_css = $start_late_css;
                                 }
