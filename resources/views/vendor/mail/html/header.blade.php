cc1cd4a64ad84e7f873626738c1f8fa5c6868f1c
jilee2 <jilee2@kissusa.com>
2022-10-28 16:11:13 -0400

    copy review due reminder test version_6

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 
-<img src="<?php echo e(asset('/storage/KEC-PM.png')); ?>" class="logo" alt="Laravel Logo" style="width: 100%; height: auto;">
+<img src="<?php echo e(asset('/storage/KEC-PM.png')); ?>" class="logo" alt="Laravel Logo" style="width: auto; height: auto;">
 
 </a>
 </td>
