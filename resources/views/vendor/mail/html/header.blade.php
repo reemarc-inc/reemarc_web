32fbf7c159709afb05f1d35ce3f8066302ed273d
jilee2 <jilee2@kissusa.com>
2022-11-04 16:55:22 -0400

    reminder email test version_14

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,8 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 
-<img src="<?php echo e(asset('/storage/KEC-PM.png')); ?>" class="logo" alt="Laravel Logo" style="width: auto; height: auto;">
+    <label style="color: #b91d19; font-size: large;">KISS PROJECT MANAGER</label>
+{{--<img src="<?php echo e(asset('/storage/KEC-PM.png')); ?>" class="logo" alt="Laravel Logo" style="width: auto; height: auto;">--}}
 
 </a>
 </td>
