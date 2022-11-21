301d7fac8601ba301964ec194e0dc24f0ff35ced
jilee2 <jilee2@kissusa.com>
2022-11-21 13:43:49 -0500

    fix bug on archives

--- app/Http/Controllers/Admin/ArchivesController.php
+++ app/Http/Controllers/Admin/ArchivesController.php
@@ -168,6 +168,8 @@ class ArchivesController extends Controller
         ];
         $this->data['assignees'] = $this->userRepository->findAll($options_);
 
+        $this->data['kiss_users'] = $this->userRepository->getKissUsers();
+        
         // Campaign_assets
         $this->data['assets'] = $assets_list = $this->campaignRepository->getAssetListById($id);
 
