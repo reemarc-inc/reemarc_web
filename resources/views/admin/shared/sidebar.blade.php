615e1d387f2aaf8bc84edf4a4fbf661ce281fcdf
jilee2 <jilee2@kissusa.com>
2023-02-03 14:54:11 -0500

    update side bar

--- resources/views/admin/shared/sidebar.blade.php
+++ resources/views/admin/shared/sidebar.blade.php
@@ -17,11 +17,11 @@
         <li class="{{ ($currentAdminMenu == 'archives') ? $activeClass : '' }}"><a class="nav-link" href="{{ url('admin/archives') }}"><i class="fas fa-archive"></i> <span>Project Archives</span></a></li>
 
         <li class="menu-header">KDO Operations Team</li>
-        <?php if(auth()->user()->team == 'KDO' || auth()->user()->team == 'Global Marketing' || auth()->user()->role == 'admin'){ ?>
-        <li class="{{ ($currentAdminMenu == 'asset_jira_kec') ? $activeClass : '' }}"><a class="nav-link" href="{{ url('admin/asset_jira_kec') }}"><i class="fas fa-th"></i> <span>Status Board (KDO)</span></a></li>
-        <?php } ?>
+
+        <li class="{{ ($currentAdminMenu == 'asset_jira_kec') ? $activeClass : '' }}"><a class="nav-link" href="{{ url('admin/asset_jira_kec') }}"><i class="fas fa-th"></i> <span>All Status Board</span></a></li>
+
         <?php if(auth()->user()->role == 'copywriter' || auth()->user()->role == 'admin'){ ?>
-        <li class="{{ ($currentAdminMenu == 'asset_jira_copywriter') ? $activeClass : '' }}"><a class="nav-link" href="{{ url('admin/asset_jira_copywriter') }}"><i class="fas fa-th"></i> <span>Status Board (C/W)</span></a></li>
+        <li class="{{ ($currentAdminMenu == 'asset_jira_copywriter') ? $activeClass : '' }}"><a class="nav-link" href="{{ url('admin/asset_jira_copywriter') }}"><i class="fas fa-th"></i> <span>Status Board (Copy)</span></a></li>
         <?php } ?>
 
         <li class="menu-header">Creative Dept</li>
