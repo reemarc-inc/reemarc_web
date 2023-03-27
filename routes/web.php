62d333f6580b23d092664b0d03e7417f669e7bf8
jilee2 <jilee2@kissusa.com>
2023-03-27 15:43:14 -0400

    forgot password v4

--- routes/web.php
+++ routes/web.php
@@ -43,8 +43,8 @@ Route::get('/email', function() {
 
 //Route::get('forget-password', [ForgotPasswordController::class, 'showForgetPasswordForm'])->name('forget.password.get');
 //Route::post('forget-password', [ForgotPasswordController::class, 'submitForgetPasswordForm'])->name('forget.password.post');
-//Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
-//Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
+Route::get('reset-password/{token}', [ForgotPasswordController::class, 'showResetPasswordForm'])->name('reset.password.get');
+Route::post('reset-password', [ForgotPasswordController::class, 'submitResetPasswordForm'])->name('reset.password.post');
 
 
 Route::get('/email_test', [SendMail::class, 'email_send'])->name('email_send');
