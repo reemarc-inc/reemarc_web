ca1477dc85eb0695e0e9b759db63aa81487d2e78
jilee2 <jilee2@kissusa.com>
2023-03-27 14:27:07 -0400

    forgot password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,6 +50,8 @@
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
+
+
                             </div>
                         </div>
 
@@ -59,11 +61,11 @@
                                     {{ __('Login') }}
                                 </button>
 
-{{--                                @if (Route::has('password.request'))--}}
-{{--                                    <a class="btn btn-link" href="{{ route('password.request') }}">--}}
-{{--                                        {{ __('Forgot Your Password?') }}--}}
-{{--                                    </a>--}}
-{{--                                @endif--}}
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
                             </div>
                         </div>
                     </form>
