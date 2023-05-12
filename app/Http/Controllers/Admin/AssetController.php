b187a4ca47d60bb6b0b19209bbb09ab33dbd2fc6
jilee2 <jilee2@kissusa.com>
2023-05-12 13:46:44 -0400

    from to on KPI

--- app/Http/Controllers/Admin/AssetController.php
+++ app/Http/Controllers/Admin/AssetController.php
@@ -171,6 +171,8 @@ class AssetController extends Controller
         $str = !empty($params['q']) ? $params['q'] : '';
         $asset_id = !empty($params['asset_id']) ? $params['asset_id'] : '';
         $campaign_id = !empty($params['campaign_id']) ? $params['campaign_id'] : '';
+        $search_from = !empty($params['search_from']) ? $params['search_from'] : '';
+        $search_to = !empty($params['search_to']) ? $params['search_to'] : '';
         if(isset($_GET['designer'])){
             $designer = $params['designer'];
         }else{
@@ -178,7 +180,7 @@ class AssetController extends Controller
         }
         $this->data['designer'] = $designer;
         $this->data['designers'] = $this->userRepository->getCreativeAssignee();
-        $this->data['asset_list'] = $this->campaignAssetIndexRepository->get_kpi_assets_list($str, $asset_id, $campaign_id, $designer);
+        $this->data['asset_list'] = $this->campaignAssetIndexRepository->get_kpi_assets_list($str, $asset_id, $campaign_id, $designer, $search_from, $search_to);
         $this->data['filter'] = $params;
 
         return view('admin.asset.kpi', $this->data);
