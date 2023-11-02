d8375a0b5a4b4710a58a79ceea1667968cc1c90f
jilee2 <jilee2@kissusa.com>
2023-11-02 16:32:54 -0400

    new server warming up

--- resources/views/admin/asset_lead_time/index.blade.php
+++ resources/views/admin/asset_lead_time/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <section class="section">
     <div class="section-header">
-        <h1>Asset Lead Time</h1>
+        <h1>Asset Lead Time *</h1>
         <div class="section-header-breadcrumb">
             <div class="breadcrumb-item"><a href="{{ url('admin/dashboard') }}">Dashboard</a></div>
             <div class="breadcrumb-item active">Asset Lead Time</div>
