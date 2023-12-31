ac97a9d2e9676daaccee9a34395a51b92b31c699
jilee2 <jilee2@kissusa.com>
2023-12-30 22:58:05 -0500

    image 2

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -365,11 +365,11 @@ class UserController extends Controller
 //                    }
 //                }
                 $image_obj = $request->file('image');
-//                foreach ($image_obj as $file){
-//                    $originalName = $file->getClientOriginalName();
-//                    $param['image'] = $originalName;
-//                }
-                $param['image'] = "/123/123";
+                $param['image'] = '';
+                foreach ($image_obj as $jin){
+                    $originalName = $jin->getClientOriginalName();
+                    $param['image'] = $originalName;
+                }
                 $user = $this->userRepository->update($user_obj['id'], $param);
                 if($user){
                     $data = [
