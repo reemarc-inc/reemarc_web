c259324f4c2f6c7544126573e2414c174ccde5f5
jilee2 <jilee2@kissusa.com>
2023-12-31 22:14:36 -0500

    image 11

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -340,6 +340,8 @@ class UserController extends Controller
         try{
             $param = $request->all();
 
+            return response()->json($request);
+
             $file = $request->file('image');
             $originalName = $file->getClientOriginalName();
             return response()->json($originalName);
