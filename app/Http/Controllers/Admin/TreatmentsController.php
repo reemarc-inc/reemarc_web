826c753689deee0dab65613f0f8896b3998b32cd
jilee2 <jilee2@kissusa.com>
2024-03-26 09:24:50 -0400

    add log for test more

--- app/Http/Controllers/Admin/TreatmentsController.php
+++ app/Http/Controllers/Admin/TreatmentsController.php
@@ -388,8 +388,8 @@ class TreatmentsController extends Controller
 
         if($sessions) {
             $first_session = $this->appointmentsRepository->get_current_session($treatment_id);
-            Log::info($treatment_id . " - " . $first_session);
-            $first_session_date = $first_session->booked_date;
+            Log::info($treatment_id . " - " . $first_session['booked_date']);
+            $first_session_date = $first_session['booked_date'];
             $this->data['last_session_status'] = $this->appointmentsRepository->get_last_treatment_session_status($treatment_id);
 
             $month_rule = [
